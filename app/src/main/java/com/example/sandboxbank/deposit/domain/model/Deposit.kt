7ac293669b64814c491e6387ca3421c394421f17
il <igurylev@yandex.ru>
2025-06-11 17:44:58 +0300

    Классы для депозита/кредита и работа с ними в Room

---
+++ app/src/main/java/com/example/sandboxbank/deposit/domain/model/Deposit.kt
@@ -0,0 +1,10 @@
+package com.example.sandboxbank.deposit.domain.model
+
+data class Deposit(
+    override val id: Int,
+    override val type: String,
+    override val openDate: Long,
+    override val percentType: Int,
+    override val period: Int,
+    override val balance: Long
+) : FinancialItem()
