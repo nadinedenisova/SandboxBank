88c06cd652311dc628c722f78f1c8add9122024e
Marat Ikhsanov <ihsmen@gmail.com>
2025-06-13 23:10:46 +0300

    Исправил Result на ResultState

--- app/src/main/java/com/example/sandboxbank/mainscreen/ui/SettingScreen.kt
+++ app/src/main/java/com/example/sandboxbank/mainscreen/ui/SettingScreen.kt
@@ -33,7 +33,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.sandboxbank.mainscreen.domain.SettingScreenViewModel
 import com.example.sandboxbank.mode.LightColorPalette
 
 
