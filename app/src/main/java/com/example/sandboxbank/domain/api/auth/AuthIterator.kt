803436ce43ff95b72d0f8a4e764d3e04e82a0df3
Marat Ikhsanov <ihsmen@gmail.com>
2025-06-17 17:14:58 +0300

    Убрать лишний state в AuthViewModel

--- app/src/main/java/com/example/sandboxbank/domain/api/auth/AuthIterator.kt
+++ app/src/main/java/com/example/sandboxbank/domain/api/auth/AuthIterator.kt
@@ -1,10 +1,11 @@
 package com.example.sandboxbank.domain.api.auth
 
+import com.example.sandboxbank.domain.model.ResultAuthState
 import kotlinx.coroutines.flow.Flow
 
 interface AuthIterator {
 
-    fun registerUser(email: String, password: String): Flow<Result<String>>
-    fun loginUser(email: String, password: String): Flow<Result<String>>
+    fun registerUser(email: String, password: String): Flow<ResultAuthState<String>>
+    fun loginUser(email: String, password: String): Flow<ResultAuthState<String>>
 
 }
