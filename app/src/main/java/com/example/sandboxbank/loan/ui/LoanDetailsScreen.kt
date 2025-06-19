c852812872096a2a5bdec31e73234f0cf6ad77bd
Marat Ikhsanov <ihsmen@gmail.com>
2025-06-19 14:29:40 +0300

    Корректировки после ревью

--- app/src/main/java/com/example/sandboxbank/loan/ui/LoanDetailsScreen.kt
+++ app/src/main/java/com/example/sandboxbank/loan/ui/LoanDetailsScreen.kt
@@ -35,33 +35,13 @@ import androidx.compose.ui.text.TextStyle
 
 @Composable
 fun LoanDetailsScreen() {
-    Column {
-        Row(modifier = Modifier.fillMaxWidth()) {
-            TopAppBar(
-                title = {
-                    Text(
-                        stringResource(R.string.loan_title), style = TextStyle(
-                            color = LightColorPalette.onSurface,
-                            fontSize = 20.sp,
-                            fontFamily = FontFamily(
-                                Font(R.font.roboto, weight = FontWeight.W400)
-                            )
-                        )
-                    )
-                },
-                navigationIcon = {
-                    IconButton(onClick = { /* handle back */ }) {
-                        Icon(
-                            painterResource(R.drawable.icon_back_arrow_16x16),
-                            contentDescription = "Back"
-                        )
-                    }
-                },
+    LoanDetailsScreenContent()
+}
 
-                backgroundColor = LightColorPalette.surface,
-                elevation = 0.dp
-            )
-        }
+@Composable
+fun LoanDetailsScreenContent() {
+    Column {
+        TopBar()
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -69,131 +49,184 @@ fun LoanDetailsScreen() {
                 .padding(40.dp)
         ) {
 
-            Column(
-                modifier = Modifier
-                    .padding(start = 20.dp)
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .padding(vertical = 10.dp)
-                ) {
-                    Text(
-                        text = "Ремонт",
-                        color = LightColorPalette.primary,
-                        fontWeight = FontWeight.Medium
-                    )
-                    IconButton(onClick = { /* handle back */ }) {
-                        Icon(
-                            painterResource(R.drawable.ic_loan_edit),
-                            contentDescription = "Редактировать",
-                            tint = LightColorPalette.primary
-                        )
-                    }
-                }
+            LoanDetailsInfo()
 
+            Spacer(modifier = Modifier.height(24.dp))
 
+            LoanDetailsButtons()
+
+            Spacer(modifier = Modifier.height(56.dp))
+
+            ScheduleReport()
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            PaymentRequired()
+        }
+
+    }
+}
+
+@Composable
+fun TopBar() {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        TopAppBar(
+            title = {
                 Text(
-                    text = "1 000 000,00 ₽",
-                    fontSize = 28.sp,
-                    fontWeight = FontWeight.Bold
-                )
-
-                Text(
-                    modifier = Modifier.padding(top = 4.dp),
-                    text = "Ставка 25% годовых",
-                    style = TextStyle(
-                        color = LightColorPalette.primary,
-                        fontSize = 16.sp,
+                    stringResource(R.string.loan_title), style = TextStyle(
+                        color = LightColorPalette.onSurface,
+                        fontSize = 20.sp,
                         fontFamily = FontFamily(
                             Font(R.font.roboto, weight = FontWeight.W400)
                         )
                     )
                 )
-            }
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(9.dp),
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Button(
-                    modifier = Modifier.weight(1f),
-                    onClick = { /* Handle payment */ },
-                    colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFEADDFF)),
-                    shape = RoundedCornerShape(16.dp)
-                ) {
-                    Text(
-                        modifier = Modifier.padding(vertical = 8.dp),
-                        text = stringResource(R.string.loan_payment),
-                        color = LightColorPalette.primary,
+            },
+            navigationIcon = {
+                IconButton(onClick = { /* handle back */ }) {
+                    Icon(
+                        painterResource(R.drawable.icon_back_arrow_16x16),
+                        contentDescription = "Back"
                     )
                 }
+            },
 
-                Button(
-                    modifier = Modifier.weight(1f),
-                    onClick = { /* Handle close */ },
-                    colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFEADDFF)),
-                    shape = RoundedCornerShape(16.dp)
-                ) {
-                    Text(
-                        modifier = Modifier.padding(vertical = 8.dp),
-                        text = stringResource(R.string.loan_close),
-                        color = LightColorPalette.primary,
-                    )
-                }
-            }
+            backgroundColor = LightColorPalette.surface,
+            elevation = 0.dp
+        )
+    }
+}
 
-            Spacer(modifier = Modifier.height(56.dp))
-
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .padding(8.dp)
-                    .fillMaxWidth()
-                    .clickable { /* Handle schedule */ }
-            ) {
+@Composable
+fun LoanDetailsInfo() {
+    Column(
+        modifier = Modifier
+            .padding(start = 20.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(vertical = 10.dp)
+        ) {
+            Text(
+                text = "Ремонт",
+                color = LightColorPalette.primary,
+                fontWeight = FontWeight.Medium
+            )
+            IconButton(onClick = { /* handle back */ }) {
                 Icon(
-                    painterResource(R.drawable.ic_loan_schedule),
-                    contentDescription = stringResource(R.string.loan_schedule),
+                    painterResource(R.drawable.ic_loan_edit),
+                    contentDescription = "Редактировать",
                     tint = LightColorPalette.primary
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(
-                    stringResource(R.string.loan_schedule),
-                    color = Color(0xFF6A4FAB),
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.SemiBold,
-                    modifier = Modifier.padding(start = 8.dp)
-                )
-            }
-
-            Spacer(modifier = Modifier.height(32.dp))
-
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .padding(8.dp)
-                    .fillMaxWidth()
-                    .clickable { /* Handle required payment */ }
-            ) {
-                Icon(
-                    painterResource(R.drawable.ic_loan_required),
-                    contentDescription = stringResource(R.string.loan_required),
-                    tint = LightColorPalette.primary
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(
-                    stringResource(R.string.loan_required),
-                    color = Color(0xFF6A4FAB),
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.SemiBold,
-                    modifier = Modifier.padding(start = 16.dp)
                 )
             }
         }
 
-    }
 
+        Text(
+            text = "1 000 000,00 ₽",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(
+            modifier = Modifier.padding(top = 4.dp),
+            text = "Ставка 25% годовых",
+            style = TextStyle(
+                color = LightColorPalette.primary,
+                fontSize = 16.sp,
+                fontFamily = FontFamily(
+                    Font(R.font.roboto, weight = FontWeight.W400)
+                )
+            )
+        )
+    }
 }
+
+@Composable
+fun LoanDetailsButtons() {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(9.dp),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Button(
+            modifier = Modifier.weight(1f),
+            onClick = { /* Handle payment */ },
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFEADDFF)),
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            Text(
+                modifier = Modifier.padding(vertical = 8.dp),
+                text = stringResource(R.string.loan_payment),
+                color = LightColorPalette.primary,
+            )
+        }
+
+        Button(
+            modifier = Modifier.weight(1f),
+            onClick = { /* Handle close */ },
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFEADDFF)),
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            Text(
+                modifier = Modifier.padding(vertical = 8.dp),
+                text = stringResource(R.string.loan_close),
+                color = LightColorPalette.primary,
+            )
+        }
+    }
+}
+
+@Composable
+fun ScheduleReport() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .clickable { /* Handle schedule */ }
+    ) {
+        Icon(
+            painterResource(R.drawable.ic_loan_schedule),
+            contentDescription = stringResource(R.string.loan_schedule),
+            tint = LightColorPalette.primary
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            stringResource(R.string.loan_schedule),
+            color = Color(0xFF6A4FAB),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
+}
+
+@Composable
+fun PaymentRequired() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .clickable { /* Handle required payment */ }
+    ) {
+        Icon(
+            painterResource(R.drawable.ic_loan_required),
+            contentDescription = stringResource(R.string.loan_required),
+            tint = LightColorPalette.primary
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            stringResource(R.string.loan_required),
+            color = Color(0xFF6A4FAB),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+    }
+}
+
+data class LoanDetailsScreenUiState(
+    val titleText: String,
+)
