64469e2d440b6782ce0b50045498248d696fc855
Konstantin <fluxgerax@gmail.com>
2025-06-27 07:28:39 +0500

    1. Наработке по бизнес логике Debit Card

--- app/src/main/java/com/example/sandboxbank/App/ui/debitcards/dto/CardResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.sandboxbank.cardmanager.cards.dto
-
-import com.example.sandboxbank.cardmanager.cards.entity.Card
-
-data class CardResponse(
-    val currentCardNumber: Long,
-    val requestNumber: Long,
-    val card: Card
-)
